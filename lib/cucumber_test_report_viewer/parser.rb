a7e4b7c0bfac2cc8341de2abda62d4d6da0feb29
Maksim Rukomoynikov <m.rukomoynikov@etpgpb.ru>
2019-08-02 19:12:35 +0300

    Парсер

--- lib/cucumber_test_report_viewer/parser.rb
+++ lib/cucumber_test_report_viewer/parser.rb
@@ -9,33 +9,35 @@ module CucumberTestReportViewer
     def self.parse(file_path = '')
       raise ArgumentError, 'Incorrect file path' if !File.exist?(file_path)
 
-      fetch_file(file_path)
-      generate_root_pages
+      fetch_test_file(file_path)
+      content = generate_root_pages
+      copy_generated_report(content)
+      copy_template_files
     end
 
     private
 
-    def self.fetch_file(file_path)
+    def self.fetch_test_file(file_path)
       file = File.open file_path
       @json_tests = JSON.load file
     end
 
     def self.generate_root_pages
-      layout = Slim::Template.new 'lib/templates/layout.html.slim'
-      
       content = @json_tests.map do |test_object|
-        content = Slim::Template.new('lib/templates/test.html.slim').render(Object.new, test_description(test_object))
+        template = Slim::Template.new File.expand_path('../../templates/test.html.slim', __FILE__ )
+        content = template.render(Object.new, test_description(test_object))
         content
       end
 
+      content
+    end
+
+    def self.copy_generated_report(content)
+      layout = Slim::Template.new File.expand_path('../../templates/layout.html.slim',__FILE__ )
       test_html = layout.render { content.join('') }
-
       Dir.mkdir('build') if !Dir.exist?('build')
-      
-
       myfile = File.new("build/test.html", "w+")
       myfile.puts(test_html)
-      FileUtils.copy_file('lib/templates/styles.css', 'build/styles.css', preserve = false, dereference = true)
     end
 
     def self.test_description(test_object)
@@ -43,13 +45,30 @@ module CucumberTestReportViewer
       test[:name] = test_object['name']
       test[:elements] = test_object['elements'].map do |element|
         {
-          keyword: element['keyword'],
-          type: element['type'],
-          steps: element['steps']
+            keyword: element['keyword'],
+            type: element['type'],
+            steps: element['steps'],
+            name: element['name']
         }
       end
 
       test
+    end
+
+    def self.copy_template_files
+      FileUtils.copy_file(
+          File.expand_path('../../templates/styles.css', __FILE__ ),
+          'build/styles.css',
+          preserve = false,
+          dereference = true
+      )
+
+      FileUtils.copy_file(
+          File.expand_path('../../templates/application.js', __FILE__ ),
+          'build/application.js',
+          preserve = false,
+          dereference = true
+      )
     end
   end
 end
--- spec/cucumber_test_report_viewer_spec.rb
+++ spec/cucumber_test_report_viewer_spec.rb
@@ -1,21 +1,8 @@
 RSpec.describe CucumberTestReportViewer::Parser do
-  # it "has a version number" do
-  #   expect(CucumberTestReportViewer::VERSION).not_to be nil
-  # end
-
-  # context "failure" do
-  #   describe '.parse' do
-  #     it "throw an error when provided file not exist" do
-  #       expect { CucumberTestReportViewer::Parser.parse('') }.to raise_error(ArgumentError)
-  #     end
-  #   end
-  # end
-
   context 'success' do
     describe '.parse' do
       it "throw an error when provided file not exist" do
         CucumberTestReportViewer::Parser.parse('spec/fixtures/elk.json')
-        # expect(CucumberTestReportViewer::Parser.parse('spec/fixtures/elk.json')).to eq {}
       end
     end
   end
